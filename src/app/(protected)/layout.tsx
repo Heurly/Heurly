2b25b003fdb15dbf488ea0066322376b8f9f807f
domesday <m.andriamiraho@gmail.com>
2024-02-10 12:47:46 +0100

    fix: redirect on login
    redirect to timetable when user is log

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,10 +1,10 @@
 import { getServerAuthSession } from "@/server/auth";
 import type { DefaultSession } from "next-auth";
-import { redirect } from "next/navigation";
+import { RedirectType, redirect } from "next/navigation";
 import type { ReactElement } from "react";
 
 export default async function ProtectedLayout({ children }: { children: React.ReactNode }): Promise<ReactElement> {
     const session: DefaultSession | null = await getServerAuthSession();
-    if (!session) redirect("/login");
+    if (!session) redirect("/login", RedirectType.replace);
     return (<>{children}</>);
 }
