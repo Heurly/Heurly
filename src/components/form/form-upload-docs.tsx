00546505456ba4a4ee6ed3fbf144ec86efefe87b
domesday <m.andriamiraho@gmail.com>
2024-02-23 13:54:48 +0100

    wip: form validation

--- src/components/form/form-upload-docs.tsx
+++ src/components/form/form-upload-docs.tsx
@@ -1,59 +1,67 @@
 "use client";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
-import { uploadFile } from "@/server/b2";
+
 import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { fileFormDocsSchema } from "@/types/fileUpload";
 import {
     Form,
+    FormControl,
     FormField,
     FormItem,
     FormLabel,
     FormMessage,
 } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-type FileFormValues = z.infer<typeof fileFormDocsSchema>;
+const formSchema = z.object({
+    file: z.instanceof(FileList),
+});
 
-export default function FormUploadDocs() {
-    const form = useForm<FileFormValues>({
-        resolver: zodResolver(fileFormDocsSchema),
+export default function Home() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
     });
 
-    const onSubmit = async (data: FileFormValues) => {
-        const file = data.file as File;
-        const res = await uploadFile(file);
+    const fileRef = form.register("file");
 
-        if (res?.error) {
-            console.error(res.error);
+    const onSubmit = (data: z.infer<typeof formSchema>) => {
+        // verification for file size and type with zod
+        try {
+            formSchema.parse(data);
+        } catch (e) {
+            form.setError("file", {
+                message: "File is too big or not a pdf.",
+            });
+            return;
         }
     };
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
-                <div className="grid w-full max-w-sm items-center gap-1.5">
-                    <FormField
-                        control={form.control}
-                        name="file"
-                        render={({ field }) => (
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="w-full p-10"
+            >
+                <FormField
+                    control={form.control}
+                    name="file"
+                    render={({ field }) => {
+                        return (
                             <FormItem>
-                                <FormLabel>Votre meilleur fichier</FormLabel>
-                                <Input
-                                    type="file"
-                                    onChange={field.onChange}
-                                    onBlur={field.onBlur}
-                                    disabled={field.disabled}
-                                    name={field.name}
-                                    ref={field.ref}
-                                />
+                                <FormLabel>File</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        type="file"
+                                        placeholder="shadcn"
+                                        {...fileRef}
+                                    />
+                                </FormControl>
                                 <FormMessage />
                             </FormItem>
-                        )}
-                    />
-                </div>
+                        );
+                    }}
+                />
                 <Button type="submit">Submit</Button>
             </form>
         </Form>
--- src/types/fileUpload.ts
+++ src/types/fileUpload.ts
@@ -1,17 +1,22 @@
 import * as z from "zod";
 
 export const fileFormDocsSchema = z.object({
-    file: z
-        .object({
-            size: z.number(),
-            type: z.string(),
-        })
-        .refine((value) => value.type === "application/pdf", {
-            message: "Le fichier doit être au format PDF",
-            path: ["file"], // Set the error path to 'file'
-        })
-        .refine((value) => value.size <= 5000000, {
-            message: "La taille du fichier ne doit pas dépasser 5MB",
-            path: ["file"], // Set the error path to 'file'
-        }),
+    file: z.object({
+        name: z.string(),
+        size: z.number().max(5000000, {
+            message: "File is too big",
+        }), // 5mb
+        type: z.string().and(
+            z.string().refine(
+                (value) => {
+                    value === "application/pdf";
+                },
+                {
+                    message: "File must be a pdf",
+                },
+            ),
+        ), // only pdf
+        lastModified: z.number(),
+        lastModifiedDate: z.date(),
+    }),
 });
