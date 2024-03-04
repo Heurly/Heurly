dcc3e55deb24ec2b1ce385071664c751eaad10b5
domesday <m.andriamiraho@gmail.com>
2024-03-04 18:11:51 +0100

    fix: error on the timetable

--- src/components/timetable/event-content.tsx
+++ src/components/timetable/event-content.tsx
@@ -1,29 +1,16 @@
 import { TEventInfo } from "@/types/timetable";
-import React from "react";
-import cn from "classnames";
 
-type PropsEventCotnent = TEventInfo & {
-    className?: string;
-};
-
-export const EventContent = React.forwardRef<HTMLDivElement, PropsEventCotnent>(
-    ({ event, timeText, className }, ref) => {
-        return (
-            <div
-                onClick={() => alert(event.title + " " + timeText)}
-                className={cn(
-                    "border-sky-100r flex h-full w-full cursor-pointer flex-col items-center justify-center rounded-xl border bg-sky-200 p-5 text-black",
-                    className,
-                )}
-                ref={ref}
-            >
-                <p className="text-center font-bold">{event.title}</p>
-                {/* <p>{timeText}</p> */}
-                <p className="text-sm">{event.extendedProps.room}</p>
-            </div>
-        );
-    },
-);
-EventContent.displayName = "EventContent";
-
-export default EventContent;
+export default function EventContent(eventInfo: TEventInfo) {
+    return (
+        <div
+            onClick={() =>
+                alert(eventInfo.event.title + " " + eventInfo.timeText)
+            }
+            className="border-sky-100r flex h-full w-full cursor-pointer flex-col items-center justify-center rounded-xl border bg-sky-200 p-5 text-black"
+        >
+            <p className="text-center font-bold">{eventInfo.event.title}</p>
+            {/* <p>{eventInfo.timeText}</p> */}
+            <p className="text-sm">{eventInfo.event.extendedProps.room}</p>
+        </div>
+    );
+}
