12c1769950073930509c0a28f3de7de2a1d3f35e
domesday <m.andriamiraho@gmail.com>
2024-03-07 13:27:51 +0100

    test new rules precommit

--- src/app/(protected)/(navbarLayout)/revision/docs/[id]/page.tsx
+++ src/app/(protected)/(navbarLayout)/revision/docs/[id]/page.tsx
@@ -39,6 +39,7 @@ export default async function PageDocs({ params }: { params: { id: string } }) {
     const blobURL = URL.createObjectURL(blob);
     // pdf type
     // Create File variable
+    let e;
     return (
         <>
             <Card className="flex h-full w-full flex-col gap-5 p-10">
