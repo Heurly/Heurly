e8ec0884cfa60e895ab576d951e1c28f469dfe31
RaphBhz <raphael.bouchez@etu.u-paris.fr>
2024-04-04 17:05:41 +0200

    remove unused imports

--- src/app/(protected)/(navbarLayout)/editor/[id]/page.tsx
+++ src/app/(protected)/(navbarLayout)/editor/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Input } from "@/components/ui/input";
 import { getNotes, updateNotes } from "@/server/notes";
