9ae110f5d15af6e30d63a645d20d43090aacd48d
agiordanoge1 <angelo.giordano@edu.esiee.com>
2024-04-11 15:38:54 +0200

    onclick detect

--- src/components/course-card.tsx
+++ src/components/course-card.tsx
@@ -1,15 +1,19 @@
+"use client";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Folder } from "lucide-react";
 
 
 interface props {
-    name: string
+    name: string;
 }
 
 export function CourseCard({ data }: { data: props }) {
+    const onClick = () => {
+        console.log("Youhou");
+    }
 
     return (
-        <Card className="flex mx-2 my-2 bg-secondary !rounded-xl ">
+        <Card onClick={onClick} className="flex mx-2 my-2 bg-secondary !rounded-xl ">
             <div className="flex gap-4 px-4 py-2 wrap align items-center">
                 <Folder size={32} />
                 <p className="w-48 text-ellipsis overflow-hidden text-nowrap">{data.name}</p>
