abdbe58eabff46d935ff952a53da2997b77dafb0
DOMESDAY <79838340+DOMESDAY7@users.noreply.github.com>
2024-04-26 16:45:28 +0200

    hide the count of selected row for now

--- src/components/data-table-pagination.tsx
+++ src/components/data-table-pagination.tsx
@@ -25,8 +25,8 @@ export function DataTablePagination<TData>({
     return (
         <div className="flex items-center justify-between px-2">
             <div className="flex-1 text-sm text-muted-foreground">
-                {table.getFilteredSelectedRowModel().rows.length}/
-                {table.getFilteredRowModel().rows.length}
+                {/* {table.getFilteredSelectedRowModel().rows.length}/
+                {table.getFilteredRowModel().rows.length} */}
             </div>
             <div className="flex items-center space-x-6 lg:space-x-8">
                 <div className="flex items-center space-x-2">
