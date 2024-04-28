e51cdcc1edd9a5b76b1e1b3de0d3805c1a8f9981
Ait Hamid Adam <106628849+aithamid@users.noreply.github.com>
2024-04-28 18:04:35 +0200

    Generate Metadata

--- src/app/(protected)/(navbarLayout)/revision/docs/[id]/page.tsx
+++ src/app/(protected)/(navbarLayout)/revision/docs/[id]/page.tsx
@@ -1,158 +1,43 @@
-"use client";
-import { notFound } from "next/navigation";
-import { Card } from "@/components/ui/card";
-import React, { useEffect } from "react";
-import { useState } from "react";
-import { getDocById, getFile } from "@/server/docs";
-import type { Docs } from "@prisma/client";
-import { SessionProvider, useSession } from "next-auth/react";
-import NotesVisibility from "@/components/docs/NotesVisibility";
-import { Switch } from "@/components/ui/switch";
-import UserProfile from "@/components/profile/UserProfile";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { Download, ExternalLink } from "lucide-react";
+import { ContentDocs } from "./content";
+import { env } from "@/env";
+import { getDocById } from "@/server/docs";
+import { Metadata } from "next";
 
+export async function generateMetadata({
+    params,
+}: {
+    params: { id: string };
+}): Promise<Metadata> {
+    const doc = await getDocById(params.id);
+    const url = `${env.NEXTAUTH_URL}/api/?doc=${params.id}`;
+    return {
+        title: doc.title,
+        alternates: {
+            canonical: `${env.NEXTAUTH_URL}/revision/docs/${params.id}`,
+        },
+        openGraph: {
+            title: doc.title,
+            type: "article",
+            description: doc.description,
+            url: `${env.NEXTAUTH_URL}/revision/docs/${params.id}`,
+            images: [
+                {
+                    url: url,
+                    width: 1200,
+                    height: 600,
+                    alt: doc.title,
+                },
+            ],
+        },
+        twitter: {
+            card: "summary_large_image",
+            site: "@heurly",
+            title: doc.title,
+            description: doc.description,
+            images: [url],
+        },
+    };
+}
 export default function Page({ params }: { params: { id: string } }) {
-    return (
-        <SessionProvider>
-            <ContentDocs docId={params.id} />
-        </SessionProvider>
-    );
-}
-
-function DocsInfo({ doc, url }: { doc: Docs; url: string }) {
-    const session = useSession();
-    return (
-        <div className="size-full space-y-4">
-            <div className=" md:h-34 flex flex-col items-center justify-between gap-2 p-4 md:flex-row ">
-                <div className="flex items-center gap-5 md:w-5/6">
-                    <h1 className="text-2xl font-bold">{doc?.title}</h1>
-                </div>
-                <div className="flex min-h-min w-1/6 items-center justify-center gap-4 md:justify-end">
-                    {session.data?.user.id === doc?.userId ? (
-                        <>
-                            <NotesVisibility isPublic={doc?.public} />
-                            <Switch
-                                disabled={session.data?.user.id !== doc?.userId}
-                                checked={doc?.public}
-                            />
-                        </>
-                    ) : (
-                        <UserProfile userId={doc?.userId} />
-                    )}
-                </div>
-            </div>
-            <div className="space-y-4">
-                <div>
-                    <h2 className="text-xl font-bold">Description</h2>
-                </div>
-                <div>
-                    <p>{doc?.description}</p>
-                </div>
-            </div>
-            <div>
-                <h2 className=" text-base font-bold text-gray-400">
-                    Matières associés
-                </h2>
-                <div className="h-8 w-16 rounded-xl bg-sky-300"></div>
-            </div>
-            <div>
-                <div className="flex size-full flex-col gap-4">
-                    <div className="md:hidden ">
-                        <Link href={url}>
-                            <Button className="size-full">
-                                <ExternalLink size={24} />
-                                <p> Ouvrir</p>
-                            </Button>
-                        </Link>
-                    </div>
-                    <div>
-                        <Button
-                            className="size-full"
-                            onClick={
-                                // download pdf
-                                () => {
-                                    const a = document.createElement("a");
-                                    a.href = url;
-                                    a.download = doc.title;
-                                    a.click();
-                                }
-                            }
-                        >
-                            <Download size={24} />
-                            <p> Télécharger</p>
-                        </Button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
-
-function ContentDocs({ docId }: { docId: string }) {
-    const [doc, setDoc] = useState<Docs>();
-    const [pdfUrl, setPdfUrl] = useState<string>("");
-
-    useEffect(() => {
-        const fetchDocs = async () => {
-            let fetchedDoc = null;
-            let fetchedBlob = null;
-            // Fetch doc from db
-            try {
-                fetchedDoc = await getDocById(docId);
-                setDoc(fetchedDoc);
-            } catch (error) {
-                console.error("Doc not found :", error);
-            }
-
-            if (!fetchedDoc) {
-                return notFound();
-            }
-
-            // Fetch pdf from bucket
-            try {
-                const json = await getFile(fetchedDoc);
-                if (!json) {
-                    throw new Error("Failed to fetch docs : no json");
-                }
-                const parsed = JSON.parse(json) as { blob: string };
-                const buffer = Buffer.from(parsed.blob, "base64");
-                fetchedBlob = new Blob([buffer], {
-                    type: "application/pdf",
-                });
-
-                // Create url for pdf
-                const url = URL.createObjectURL(fetchedBlob);
-                setPdfUrl(url);
-            } catch (error) {
-                console.error("Failed to fetch doc:", error);
-            }
-        };
-
-        void fetchDocs();
-    }, [setDoc, docId, setPdfUrl]);
-
-    return (
-        <>
-            {doc && doc.public && doc.userId && pdfUrl !== "" && (
-                <div className="size-full gap-y-5">
-                    <Card className="flex h-full flex-col gap-5 p-4 md:flex-row md:p-10">
-                        <div className=" h-full md:w-1/2">
-                            <DocsInfo doc={doc} url={pdfUrl} />
-                        </div>
-                        <div className="hidden border md:block md:w-1/2">
-                            <iframe
-                                src={pdfUrl}
-                                title={doc?.title}
-                                height="100%"
-                                width="100%"
-                                className="aspect-auto size-full flex-col"
-                            ></iframe>
-                        </div>
-                    </Card>
-                </div>
-            )}
-        </>
-    );
+    return <ContentDocs docId={params.id} />;
 }
--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,90 +1,106 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable-next-line @next/next/no-img-element */
+
 import { getDocById } from "@/server/docs";
 import { getUserPublicInfo } from "@/server/user";
 import { Docs } from "@prisma/client";
-import { notFound } from "next/navigation";
 import { ImageResponse } from "next/og";
 
-export async function GET({ document }: { document: Docs }) {
-    if (document) {
-        return new ImageResponse(
-            (
-                <div
-                    style={{
-                        fontSize: 128,
-                        background: "white",
-                        width: "100%",
-                        height: "100%",
-                        display: "flex",
-                        textAlign: "center",
-                        alignItems: "center",
-                        justifyContent: "center",
-                    }}
-                >
-                    {document.title}
-                </div>
-            ),
-            {
-                width: 1200,
-                height: 600,
-            },
-        );
-    } else {
-        const docId = "clv2mrwnx0001h24gh0b5k4uk";
-        let fetchedDoc = null;
-        try {
-            fetchedDoc = await getDocById(docId);
-        } catch (error) {
-            console.error("Doc not found :", error);
-        }
-        if (!fetchedDoc) {
-            return notFound();
-        }
+export async function GET(request: Request): Promise<ImageResponse> {
+    const { searchParams } = new URL(request.url);
+    const doc = searchParams.get("doc");
 
-        const user = await getUserPublicInfo(fetchedDoc.userId);
+    if (!doc) {
+        return NotFound();
+    }
 
-        if (!user || user.name === "" || user.image === "") {
-            return notFound();
-        }
-        return new ImageResponse(
-            (
-                <div
-                    tw="p-6 h-full w-full flex justify-center items-center"
-                    style={{
-                        background:
-                            "linear-gradient(133deg, rgb(56, 182, 212) 0%, rgb(59, 130, 246) 55%, rgb(255, 255, 255) 100%)",
-                        // fontFamily: '"Josefin-Sans"',
-                    }}
-                >
-                    <div tw="rounded p-10 bg-zinc-900 h-full w-full flex flex-col">
-                        <div tw="mt-10 mb-3 flex items-center">
-                            <img
-                                width="84"
-                                height="84"
-                                src={user.image}
-                                alt="user profile picture"
-                                tw="border-2 border-white rounded-full"
-                            />
-                        </div>
-                        <div tw="mt-16 flex text-6xl leading-normal text-gray-200">
-                            {fetchedDoc.title}
-                        </div>
-                        <div tw="mt-5 flex text-3xl text-gray-300">
-                            {fetchedDoc.description}
-                        </div>
-                        <div tw="mt-5 flex items-center text-xl text-gray-300">
-                            <div>{user.name}</div>
-                            <div tw="-mt-2 ml-3">.</div>
-                            <div tw="ml-3">
-                                {fetchedDoc.updatedAt.toLocaleDateString()}
-                            </div>
+    const document = await getDocById(doc);
+    if (!document) {
+        return NotFound();
+    }
+
+    const user = await getUserPublicInfo(document.userId);
+    if (!user) {
+        return NotFound();
+    }
+
+    return Found({ document, user });
+}
+
+async function Found({
+    document,
+    user,
+}: {
+    document: Docs;
+    user: { name: string | null; image: string | null };
+}) {
+    if (user.image === null) {
+        user.image = "";
+    }
+    return new ImageResponse(
+        (
+            <div
+                tw="p-6 h-full w-full flex justify-center items-center"
+                style={{
+                    background:
+                        "linear-gradient(133deg, rgb(56, 182, 212) 0%, rgb(59, 130, 246) 55%, rgb(255, 255, 255) 100%)",
+                    // fontFamily: '"Josefin-Sans"',
+                }}
+            >
+                <div tw="rounded p-10 bg-zinc-900 h-full w-full flex flex-col">
+                    <div tw="mt-10 mb-3 flex items-center">
+                        <img
+                            width="84"
+                            height="84"
+                            src={user.image}
+                            alt="user profile picture"
+                            tw="border-2 border-white rounded-full"
+                        />
+                    </div>
+                    <div tw="mt-16 flex text-6xl leading-normal text-gray-200">
+                        {document.title}
+                    </div>
+                    <div tw="mt-5 flex text-3xl text-gray-300">
+                        {document.description}
+                    </div>
+                    <div tw="mt-5 flex items-center text-xl text-gray-300">
+                        <div>{user.name}</div>
+                        <div tw="-mt-2 ml-3">.</div>
+                        <div tw="ml-3">
+                            {document.updatedAt.toLocaleDateString()}
                         </div>
                     </div>
                 </div>
-            ),
-            {
-                width: 1200,
-                height: 600,
-            },
-        );
-    }
+            </div>
+        ),
+        {
+            width: 1200,
+            height: 600,
+        },
+    );
+}
+
+async function NotFound() {
+    return new ImageResponse(
+        (
+            <div
+                tw="p-6 h-full w-full flex justify-center items-center"
+                style={{
+                    background:
+                        "linear-gradient(133deg, rgb(246, 030, 033) 0%, rgb(255, 010, 000) 55%, rgb(0, 0, 0) 100%)",
+                    // fontFamily: '"Josefin-Sans"',
+                }}
+            >
+                <div tw="rounded p-10 bg-zinc-900 h-full w-full flex flex-col">
+                    <div tw="mt-16 flex text-6xl leading-normal text-gray-200">
+                        Not found
+                    </div>
+                </div>
+            </div>
+        ),
+        {
+            width: 1200,
+            height: 600,
+        },
+    );
 }
