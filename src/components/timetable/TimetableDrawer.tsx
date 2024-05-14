30b22941518cee7c3c878e43ef00ac0211c3ef91
Ait Hamid Adam <aithamidbusiness@gmail.com>
2024-05-14 13:37:37 +0200

    Fix editor and show notes timetable

--- src/components/timetable/TimetableDrawer.tsx
+++ src/components/timetable/TimetableDrawer.tsx
@@ -1,4 +1,8 @@
-import { createNotes, getCourseDateNotes } from "@/server/notes";
+import {
+    createNotes,
+    getCourseDateNotesMy,
+    getCourseDateNotesPublic,
+} from "@/server/notes";
 import { getCourseDataQuestions } from "@/server/question";
 import type { TEventClickArg } from "@/types/timetable";
 import type { Notes, Question } from "@prisma/client";
@@ -48,7 +52,8 @@ const TimetableDrawer: React.FunctionComponent<Props> = ({
     setOpen,
 }) => {
     const router = useRouter();
-    const [notes, setNotes] = useState<Notes[]>([]);
+    const [notesPublic, setNotesPublic] = useState<Notes[]>([]);
+    const [notesMy, setNotesMy] = useState<Notes[]>([]);
     const [questions, setQuestions] = useState<Question[]>([]);
     const [isMobile, setIsMobile] = useState<boolean>(
         window.innerWidth < nbPxPhone,
@@ -91,11 +96,15 @@ const TimetableDrawer: React.FunctionComponent<Props> = ({
                 eventInfo.event._instance.range.start.getTimezoneOffset() *
                     60000,
         );
-
-        void getCourseDateNotes({
+        void getCourseDateNotesPublic({
             courseId: eventInfo.event._def.extendedProps.courseId,
             courseDate: correctedCourseDate,
-        }).then((r) => setNotes(r ?? []));
+        }).then((r) => setNotesPublic(r ?? []));
+
+        void getCourseDateNotesMy({
+            courseId: eventInfo.event._def.extendedProps.courseId,
+            courseDate: correctedCourseDate,
+        }).then((r) => setNotesMy(r ?? []));
 
         void getCourseDataQuestions({
             courseId: eventInfo.event._def.extendedProps.courseId,
@@ -173,24 +182,41 @@ const TimetableDrawer: React.FunctionComponent<Props> = ({
                         </div>
                         <Separator className="my-5 border-b" />
                         <div className="flex w-full flex-col">
-                            <p className="text-xl font-bold">
-                                Ils ont pris des notes :{" "}
-                            </p>
-                            {notes?.length > 0 &&
-                                notes.map((n) => (
+                            {notesMy?.length > 0 && (
+                                <p className="text-xl font-bold">Mes notes :</p>
+                            )}
+                            {notesMy?.length > 0 &&
+                                notesMy.map((n) => (
                                     <ItemsLink
                                         key={ID()}
                                         title={n.title}
                                         link={`/editor/${n.id}`}
                                     />
                                 ))}
-                            <Button
-                                onClick={createNotesAndRedirect}
-                                className="mt-4"
-                            >
-                                <Pencil className="mr-2" />
-                                <p>Prendre des notes</p>
-                            </Button>
+
+                            {notesPublic?.length > 0 && (
+                                <p className="text-xl font-bold">
+                                    Ils ont pris des notes :{" "}
+                                </p>
+                            )}
+
+                            {notesPublic?.length > 0 &&
+                                notesPublic.map((n) => (
+                                    <ItemsLink
+                                        key={ID()}
+                                        title={n.title}
+                                        link={`/editor/${n.id}`}
+                                    />
+                                ))}
+                            {notesMy?.length === 0 && (
+                                <Button
+                                    onClick={createNotesAndRedirect}
+                                    className="mt-4"
+                                >
+                                    <Pencil className="mr-2" />
+                                    <p>Prendre des notes</p>
+                                </Button>
+                            )}
                         </div>
                         <Separator className="my-5 border-b" />
                         <div className="flex w-full flex-col">
